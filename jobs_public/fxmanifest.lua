24c4b6a4530ea6614304a2dd7f929c65849dbc68
Arthur Pequeno <68912054+Zhawty@users.noreply.github.com>
2021-05-04 16:06:26 -0300

    Update fxmanifest.lua

--- jobs_public/fxmanifest.lua
+++ jobs_public/fxmanifest.lua
@@ -1,14 +1,16 @@
 fx_version 'adamant'
 game 'gta5'
 
+author 'https://github.com/Zhawty/'
+
 client_scripts {
 	'@vrp/lib/utils.lua',
-    'config/*.lua',
+    	'config/*.lua',
 	'client/*.lua'
 }
 
 server_scripts {
 	'@vrp/lib/utils.lua',
-    'config/*.lua',
+    	'config/*.lua',
 	'server/*.lua'
 }
